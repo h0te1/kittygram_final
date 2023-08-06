e4f398b0312194ab11b89ce0b31d32d13c3e1cd1
Никита Зайцев <genius.zaytsev@gmail.com>
2023-08-06 18:58:52 +0500

    local changes

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -55,11 +55,16 @@ WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': os.getenv('POSTGRES_DB', 'kittygram'),
+       'USER': os.getenv('POSTGRES_USER', 'kittygram_user'),
+       'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'kittygram_password'),
+       'HOST': os.getenv('DB_HOST', 'db'),
+       'PORT': os.getenv('DB_PORT', 5432)
+   }
 }
+
 
 
 # Password validation
@@ -93,7 +98,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = BASE_DIR / "collected_static"
+
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
